71cf6252e0765d164cde24781a8d57395ebedd59
vandewyr <vandewynkel@gmail.com>
2017-03-16 18:45:59 -0500

    3/16/17

--- app/src/main/java/com/gator/squadron/TowerChapterOne.java
+++ app/src/main/java/com/gator/squadron/TowerChapterOne.java
@@ -2,6 +2,7 @@ package com.gator.squadron;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class TowerChapterOne extends AppCompatActivity {
 
@@ -9,5 +10,22 @@ public class TowerChapterOne extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tower_chapter_one);
+
+        long t = (long)15000;
+        ImageView f22 = (ImageView)findViewById(R.id.f22Image);
+        proceedDownwind(f22);
+            //f22.animate().rotation(180);
+
+    }
+
+    public void proceedDownwind(ImageView aircraft){
+        float x = (float)700;
+        float y = (float)150;
+        float negX = (float)-140;
+        long t = (long)15000;
+        float enterDownwind = (float)180;
+
+        aircraft.animate().yBy(y).setDuration(t);
+        aircraft.animate().translationXBy(negX).setDuration(t);
     }
 }
